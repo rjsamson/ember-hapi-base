22a77f29eba9e3bc9e529ac3abdaa29f382c315b
Robert J Samson <rjsamson@me.com>
2014-05-07 10:24:44 -0400

    Add clear completed to todos example

--- app/scripts/controllers/todos_controller.js
+++ app/scripts/controllers/todos_controller.js
@@ -1,5 +1,11 @@
 App.TodosController = Ember.ArrayController.extend({
   newTodo: '',
+  completed: Ember.computed.filterBy('content', 'complete', true),
+
+  hasCompleted: function() {
+    return this.get('completed').get('length') > 0;
+  }.property('model.@each.complete'),
+
   actions: {
     createTodo: function() {
       var todo = this.store.createRecord('todo', {
@@ -12,8 +18,15 @@ App.TodosController = Ember.ArrayController.extend({
         controller.set('newTodo', '');
       });
     },
+
     deleteTodo: function(todo) {
       todo.destroyRecord();
+    },
+
+    clearCompleted: function() {
+      var completed = this.get('completed');
+      completed.invoke('deleteRecord');
+      completed.invoke('save');
     }
   }
 });
