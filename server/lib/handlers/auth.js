4ca73add309d63bf75e7e1c58ac1fc46c8749c55
Robert J Samson <rjsamson@me.com>
2014-05-15 11:47:15 -0400

    Implement cookie based auth

---
+++ server/lib/handlers/auth.js
@@ -0,0 +1,37 @@
+// Hard coded user for demo purposes
+
+var users = {
+  admin: {
+    id: 'admin',
+    password: 'password',
+    name: 'Admin'
+  }
+}
+
+exports.Login = function(request, reply) {
+  if (request.auth.isAuthenticated) {
+    return reply({success: true});
+  }
+
+  var account = null;
+
+  if (!request.payload.username || !request.payload.password) {
+    return reply({error: "invalid username or password"})
+             .code(401);
+  } else {
+    account = users[request.payload.username];
+
+    if (!account || account.password !== request.payload.password) {
+      return reply({error: "invalid username or password"})
+             .code(401);
+    }
+  }
+
+  request.auth.session.set(account);
+  return reply({success: true});
+}
+
+exports.Logout = function(request, reply) {
+  request.auth.session.clear();
+  return reply({success: true});
+}
--- server/lib/server.js
+++ server/lib/server.js
@@ -1,6 +1,26 @@
 var Hapi = require('hapi');
 var server = Hapi.createServer('localhost', 8000);
 
+// Setup auth
+var AuthHandlers = require('./handlers/auth');
+
+server.pack.require('hapi-auth-cookie', function(err) {
+  server.auth.strategy('session', 'cookie', {
+    // replace this, or better yet get it from a config file not checked into git!
+    password: 'supersecrettoken',
+    cookie: 'hapi-ember-base-sid',
+    isSecure: false
+  });
+});
+
+// Routes to manage the session
+
+server.route([
+  { method: 'POST', path: '/session', config: { handler: AuthHandlers.Login, auth: { mode: 'try' } } },
+  { method: 'DELETE', path: '/session', config: { handler: AuthHandlers.Logout, auth: true } }
+]);
+
+// Setup routes
 var todos = require('./routes/todos');
 server.route(todos);
 
